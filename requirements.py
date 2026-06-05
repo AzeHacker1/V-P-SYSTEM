af9d2238df3b0de7bf103e26538aca01ef798ae0
AzeHacker1 <95229130+AzeHacker1@users.noreply.github.com>
2026-06-06 03:35:17 +0400

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,15 @@
+termux-setup-storage
+
+pkg update && pkg upgrade -y
+
+pkg install python
+
+pkg install python2 
+
+pkg install python3 
+
+pip install telethon
+
+pkg install git
+
+pkg install termux-api -y
